5a9be3e908cfc1264202d2a52b5431fb2deae836
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2024-05-08 11:13:16 -0600

    Task #2 - Create a to-do task and handle the checks status and also add the edit field input when user double click on the label text

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   footerEl,
   taskEl,
   newTodoInputEl,
+  todoEl,
 } from "./js/utils/dom-elements";
 import newTaskItemFactory from "./js/factory/new-task-item.factory";
 
@@ -28,6 +29,48 @@ newTodoInputEl.getElement().addEventListener("keypress", (e) => {
       console.error("You can not create a task with empty title!");
     }
   }
+});
+
+const handlePendingTaskStatus = (e, taskItemEl) => {
+  if (
+    e.target.tagName === "INPUT" &&
+    e.target.getAttribute("type") === "checkbox"
+  ) {
+    if (e.target.checked) {
+      e.target.setAttribute("checked", "");
+      taskItemEl.setAttribute("class", "completed");
+    } else {
+      e.target.removeAttribute("checked");
+      taskItemEl.removeAttribute("class");
+    }
+  }
+};
+
+let mouseClicks = 0;
+let timer = null;
+
+const handleEditTaskMode = (e, taskItemEl) => {
+  if (e.target.tagName === "LABEL") {
+    const checkboxEl = e.target.previousSibling.previousSibling;
+    mouseClicks++;
+    if (mouseClicks === 1) {
+      timer = setTimeout(() => {
+        mouseClicks = 0;
+      }, 500);
+    }
+    if (mouseClicks === 2 && !checkboxEl.checked) {
+      taskItemEl.removeAttribute("class");
+      taskItemEl.setAttribute("class", "editing");
+      mouseClicks = 0;
+      clearTimeout(timer);
+    }
+  }
+};
+
+todoEl.getElement().addEventListener("click", (e) => {
+  const taskItemEl = e.target.parentNode.parentNode;
+  handlePendingTaskStatus(e, taskItemEl);
+  handleEditTaskMode(e, taskItemEl);
 });
 
 (function () {
